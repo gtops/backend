0e19c5f9876946ff9e8bbe76909b1faa048a729a
fjodor-rybakov <fedryb@mail.ru>
2019-04-22 00:21:00 +0300

    add swagger

--- src/middleware/interfaces/ERoles.ts
+++ src/middleware/interfaces/ERoles.ts
@@ -1,6 +1,6 @@
 export enum ERoles {
 	GUEST = "ГОСТЬ",
-	INVITED_USER = "",
+	INVITED_USER = "ПРИГЛАШЁННЫЙ ПОЛЬЗОВАТЕЛЬ",
 	JUDGE = "СУДЬЯ",
 	ORGANIZER = "ОРГАНИЗАТОР",
 	LOCAL_ADMIN = "ЛОКАЛЬНЫЙ АДМИНИСТРАТОР",
