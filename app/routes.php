86e918f284c0ca899e3929986d24ab10cbb589ef
islamov-jahja <yahyaislamov15@mail.ru>
2019-10-15 02:21:22 +0300

    сделал небольшой рефакторинг

--- app/routes.php
+++ app/routes.php
@@ -9,13 +9,5 @@ use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
-    $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
-        return $response;
-    });
-
-    $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
-        $group->get('/{id}', ViewUserAction::class);
-    });
+    
 };
